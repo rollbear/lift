96cbbd3e4c39769509b67b3986e0b3c541aefaac
Bj?rn Fahller <bjorn@fahller.se>
2017-12-29 10:24:36 +0100

    Stylistic changes

--- include/lift.hpp
+++ include/lift.hpp
@@ -16,7 +16,11 @@
 namespace lift {
 
 template <typename F, typename ... Fs>
-inline constexpr auto compose(F&& f, Fs&&... fs)
+inline
+constexpr
+auto compose(
+  F&& f,
+  Fs&&... fs)
 {
   if constexpr (sizeof...(fs) == 0)
   {
@@ -27,83 +31,124 @@ inline constexpr auto compose(F&& f, Fs&&... fs)
     return [f = std::forward<F>(f), tail = compose(std::forward<Fs>(fs)...)]
       (auto&& ... objs)
     {
-      using tailtype = decltype(tail);
-      constexpr bool multitail = (std::is_invocable_v<tailtype, decltype(objs)> && ...);
-      constexpr bool unitail = std::is_invocable_v<tailtype, decltype(objs)...>;
-      static_assert(sizeof...(objs) == 1 || !(multitail && unitail), "ambigous composition");
+      using tail_type = decltype(tail);
+      constexpr bool multitail = (std::is_invocable_v<tail_type, decltype(objs)>
+                                  && ...);
+      constexpr bool unitail = std::is_invocable_v<tail_type, decltype(objs)...>;
+
+      static_assert(sizeof...(objs) == 1 || !(multitail && unitail),
+                    "ambigous composition");
+
       if constexpr (unitail)
       {
-        constexpr bool handled = std::is_invocable_v<F, decltype(tail(LIFT_FWD(objs)...))>;
-        static_assert(handled, "can't compose");
-        if constexpr (handled)
+        using tail_rt = decltype(tail(LIFT_FWD(objs)...));
+        constexpr bool match = std::is_invocable_v<F, tail_rt>;
+        static_assert(match, "type mismatch, can't compose");
+        if constexpr (match)
         {
           return f(tail(LIFT_FWD(objs)...));
         }
       }
       else if constexpr(multitail)
       {
-        constexpr bool handled = std::is_invocable_v<F, decltype(tail(LIFT_FWD(objs)))...>;
-        static_assert(handled, "can't compose");
-        if constexpr (handled)
+        constexpr bool match =
+          std::is_invocable_v<F, decltype(tail(LIFT_FWD(objs)))...>;
+
+        static_assert(match, "type mismatch, can't compose");
+        if constexpr (match)
         {
           return f(tail(LIFT_FWD(objs))...);
         }
       }
       else
       {
-        static_assert(multitail || unitail, "can't compose");
+        static_assert(multitail || unitail, "arity mismatch, can't compose");
       }
     };
   }
 }
 
 template <typename F>
-inline constexpr auto negate(F&& f)
+inline
+constexpr
+auto
+negate(
+  F&& f)
 {
-  return [f = std::forward<F>(f)](auto&& ... obj)
-         LIFT_THRICE(!f(LIFT_FWD(obj)...));
+  return
+    [f = std::forward<F>(f)](auto&& ... obj) LIFT_THRICE(!f(LIFT_FWD(obj)...));
 }
 
 template <typename T>
-inline constexpr auto equals(T&& t)
+inline
+constexpr
+auto
+equals(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj == t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj == t);
 }
 
 template <typename T>
-inline constexpr auto not_equal(T&& t)
+inline
+constexpr
+auto
+not_equal(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj != t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj != t);
 }
 
 template <typename T>
-inline constexpr auto less_than(T&& t)
+inline
+constexpr
+auto
+less_than(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj < t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj < t);
 }
 
 template <typename T>
-inline constexpr auto less_equal(T&& t)
+inline
+constexpr
+auto
+less_equal(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj <= t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj <= t);
 }
 
 template <typename T>
-inline constexpr auto greater_than(T&& t)
+inline
+constexpr
+auto
+greater_than(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj > t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj > t);
 }
 
 template <typename T>
-inline auto constexpr greater_equal(T&& t)
+inline
+auto
+constexpr
+greater_equal(
+  T&& t)
 {
-  return [t = std::forward<T>(t)](const auto& obj)  LIFT_THRICE(obj >= t);
+  return [t = std::forward<T>(t)](const auto& obj) LIFT_THRICE(obj >= t);
 }
 
 namespace detail
 {
   template <typename Fs, std::size_t ... I, typename ... T>
-  inline constexpr bool when_all(Fs& fs, std::index_sequence <I...>, T&& ... t)
+  inline
+  constexpr
+  bool
+  when_all(
+    Fs& fs,
+    std::index_sequence <I...>,
+    T&& ... t)
   noexcept(noexcept((std::get<I>(fs)(std::forward<T>(t)...) && ...)))
   {
     return (std::get<I>(fs)(std::forward<T>(t)...) && ...);
@@ -111,52 +156,87 @@ namespace detail
 }
 
 template <typename ... Fs>
-inline constexpr auto when_all(Fs&&... fs)
+inline
+constexpr
+auto
+when_all(
+  Fs&&... fs)
 {
-  return [funcs = std::tuple(std::forward<Fs>(fs)...)](const auto& ... obj)
+  return
+    [funcs = std::tuple(std::forward<Fs>(fs)...)]
+      (const auto& ... obj)
     noexcept(noexcept((std::forward<Fs>(fs)(obj...) && ...)))
   -> bool
   {
-    return detail::when_all(funcs, std::index_sequence_for<Fs...>{}, LIFT_FWD(obj)...);
+    return detail::when_all(
+      funcs,
+      std::index_sequence_for<Fs...>{}, LIFT_FWD(obj)...
+    );
   };
 }
 
 namespace detail
 {
   template <typename Fs, std::size_t ... I, typename ... T>
-  inline constexpr bool when_any(Fs& fs, std::index_sequence<I...>, T&& ... t)
+  inline
+  constexpr
+  bool
+  when_any(
+    Fs& fs,
+    std::index_sequence<I...>,
+    T&& ... t)
   noexcept(noexcept((std::get<I>(fs)(std::forward<T>(t)...) || ...)))
   {
     return (std::get<I>(fs)(std::forward<T>(t)...) || ...);
   }
 }
 template <typename ... Fs>
-inline constexpr auto when_any(Fs&& ... fs)
+inline
+constexpr
+auto
+when_any(
+  Fs&& ... fs)
 {
-  return [funcs = std::tuple(std::forward<Fs>(fs)...)](const auto& ... obj)
+  return
+    [funcs = std::tuple(std::forward<Fs>(fs)...)]
+      (const auto& ... obj)
     noexcept(noexcept((std::forward<Fs>(fs)(obj...) || ...)))
   -> bool
   {
-    return detail::when_any(funcs, std::index_sequence_for<Fs...>{}, LIFT_FWD(obj)...);
+    return detail::when_any(
+      funcs,
+      std::index_sequence_for<Fs...>{}, LIFT_FWD(obj)...
+    );
   };
 }
 
 template <typename ... Fs>
-inline constexpr auto when_none(Fs ... fs)
+inline
+constexpr
+auto
+when_none(
+  Fs ... fs)
 {
   return negate(when_any(std::move(fs)...));
 }
 
 template <typename Predicate, typename Action>
-inline constexpr auto if_then(Predicate&& predicate, Action&& action)
+inline
+constexpr
+auto
+if_then(
+  Predicate&& predicate,
+  Action&& action)
 {
   return [
     predicate = std::forward<Predicate>(predicate),
-    action = std::forward<Action>(action)]
+    action = std::forward<Action>(action)
+  ]
     (auto&& ... obj)
   mutable
-    noexcept(noexcept(true == predicate(obj...))
-             && noexcept(action(LIFT_FWD(obj)...)))
+    noexcept(
+    noexcept(true == predicate(obj...))
+    && noexcept(action(LIFT_FWD(obj)...)))
     -> void
   {
     if (predicate(obj...))
@@ -167,19 +247,25 @@ inline constexpr auto if_then(Predicate&& predicate, Action&& action)
 }
 
 template <typename Predicate, typename TAction, typename FAction>
-inline constexpr auto if_then_else(Predicate&& predicate,
-                         TAction&& t_action,
-                         FAction&& f_action)
+inline
+constexpr
+auto
+if_then_else(
+  Predicate&& predicate,
+  TAction&& t_action,
+  FAction&& f_action)
 {
   return [
     predicate = std::forward<Predicate>(predicate),
     t_action = std::forward<TAction>(t_action),
     f_action = std::forward<FAction>(f_action)
-  ](auto&& ... obj)
+  ]
+    (auto&& ... obj)
   mutable
-  noexcept(noexcept(true == predicate(obj...))
-           && noexcept(t_action(LIFT_FWD(obj)...))
-           && noexcept(t_action(LIFT_FWD(obj)...)))
+    noexcept(
+    noexcept(true == predicate(obj...))
+    && noexcept(t_action(LIFT_FWD(obj)...))
+    && noexcept(t_action(LIFT_FWD(obj)...)))
   {
     if (predicate(obj...))
     {
@@ -194,23 +280,39 @@ inline constexpr auto if_then_else(Predicate&& predicate,
 
 namespace detail
 {
-template <typename Fs, std::size_t ... I, typename ... T>
-inline constexpr void do_all(Fs& fs, std::index_sequence<I...>, T&& ... t)
-{
-   ((void)(std::get<I>(fs)(std::forward<T>(t)...)),  ...);
-}
+  template <typename Fs, std::size_t ... I, typename ... T>
+  inline
+  constexpr
+  void
+  do_all(
+    Fs& fs,
+    std::index_sequence<I...>,
+    T&& ... t)
+  {
+    ((void)(std::get<I>(fs)(std::forward<T>(t)...)),  ...);
+  }
 }
 
 
 template <typename ... Fs>
-inline constexpr auto do_all(Fs ... fs)
+inline
+constexpr
+auto
+do_all(
+  Fs ... fs)
 {
-  return [funcs = std::tuple(std::forward<Fs>(fs)...)](const auto& ... obj)
-    mutable
-    noexcept(noexcept(((void)(LIFT_FWD(obj)), ...)))
+  return
+    [funcs = std::tuple(std::forward<Fs>(fs)...)]
+      (const auto& ... obj)
+      mutable
+      noexcept(noexcept(((void)(LIFT_FWD(obj)), ...)))
   -> void
   {
-    detail::do_all(funcs, std::index_sequence_for<Fs...>{}, LIFT_FWD(obj)...);
+    detail::do_all(
+      funcs,
+      std::index_sequence_for<Fs...>{},
+      LIFT_FWD(obj)...
+    );
   };
 }
 
