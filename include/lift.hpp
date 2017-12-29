43b5a54075527796d85c227b79051de51cb302b3
Bj?rn Fahller <bjorn@fahller.se>
2017-12-29 10:30:26 +0100

    Added license (boost) and copyright

--- include/lift.hpp
+++ include/lift.hpp
@@ -1,3 +1,16 @@
+/*
+ * lift C++ higher order convenience functions
+ *
+ * Copyright © Björn Fahller 2017
+ *
+ *  Use, modification and distribution is subject to the
+ *  Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * Project home: https://github.com/rollbear/lift
+ */
+
 #ifndef HIGHER_ORDER_FUNCTIONS_LIFT_HPP
 #define HIGHER_ORDER_FUNCTIONS_LIFT_HPP
 
