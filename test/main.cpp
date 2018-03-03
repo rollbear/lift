a0e7214be5ea804adc476e7147fa560c514fe8cb
Bj?rn Fahller <bjorn@fahller.se>
2018-03-03 11:05:24 +0100

    Updated copyright

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 /*
  * lift C++ higher order convenience functions
  *
- * Copyright © Björn Fahller 2017
+ * Copyright © Björn Fahller 2017,2018
  *
  *  Use, modification and distribution is subject to the
  *  Boost Software License, Version 1.0. (See accompanying
